839cab8a635f79a9fb85592d93527427f0d0428e
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-20 15:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+function printHello(){
+	return new Promise((res,rej)=>{
+
+		setTimeout(()=>{
+
+			res("Hello, world!")
+		},1000)
+	})
+}
+
+printHello.then((res)=> output.innerText = res)
+	.catch((err)=> console.log(err))
